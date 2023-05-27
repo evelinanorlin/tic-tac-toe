ebfecc617ef9281f5fcf26be5eab5b89fc2cd820
Evelina Norlin <evelina.norlin@hotmail.com>
2023-05-27 16:04:57 +0200

    started project, able to add players

---
+++ src/components/Login.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+  import { ref } from 'vue'; 
+
+  let playerO = ref('');
+  let playerX = ref('');
+
+  const emits = defineEmits<{
+    (e: string, name: string): void
+  }>()
+
+  const sendPlayerO = () => {
+    emits('playerO', playerO.value);
+  }
+
+  const sendPlayerX = () => {
+    emits('playerX', playerX.value);
+  }
+
+</script>
+
+<template>
+  <h1>Welcome to Tic-Tac-Toe!</h1>
+
+  <form>
+    <label for="playerO">Player O</label><br>
+    <input type="text" id="playerO" v-model="playerO">
+    <button @click.prevent="sendPlayerO">Spara</button>
+  </form>
+
+  <form>
+    <label for="playerX">Player X</label><br>
+    <input type="text" id="playerX" v-model="playerX">
+    <button @click.prevent="sendPlayerX">Spara</button>
+  </form>
+
+  <button disabled>Start game!</button>
+</template>
---
+++ src/components/Start.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import Login from './Login.vue';
+
+let showLogin = ref(true);
+
+let playerO = ref({name: '', score: 0})
+let playerX = ref({name: '', score: 0})
+
+const addPlayerO = (newName: string) => {
+  playerO.value.name = newName;
+  console.log(playerO);
+}
+
+const addPlayerX = (newName: string) => {
+  playerX.value.name = newName;
+  console.log(playerX);
+}
+</script>
+
+<template>
+  <div v-if="showLogin">
+    <Login @playerO="addPlayerO" @playerX="addPlayerX"/>
+  </div>
+</template>
